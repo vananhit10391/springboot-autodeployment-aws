8fdfc51ae6c86fb9d374b479120753950bc205a4
vananhit10391 <vananhit10391@gmail.com>
2021-04-26 13:48:49 +0700

    the second commit

---
+++ src/main/java/com/example/springbootautodeploymentaws/SpringbootAutodeploymentAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootautodeploymentaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutodeploymentAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAutodeploymentAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootautodeploymentaws/SpringbootAutodeploymentAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootautodeploymentaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutodeploymentAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
